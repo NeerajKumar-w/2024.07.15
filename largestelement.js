515a73636b1f16be0cb9ff439d3469faa365d9a6
NeerajKumar-w <neerajkumar2work@gmail.com>
2024-07-15 16:36:25 +0530

    Update largestelement.js

--- largestelement.js
+++ largestelement.js
@@ -1,16 +1,19 @@
 let arr = [];
-let curele = 0;
-let greatele = 0;
 let i = 0;
 let size = Number(prompt());
 
 for(i = 0; i < size; i+=1){
     arr.push(prompt());
 }
-for(let j = 0; j < size; j += 1){
-    curele = arr[j];
-    if(curele > greatele){
-        greatele = curele;
+function great(arr){
+    let a = 0;
+    let b = 0;
+    for(let j = 0; j < size; j += 1){
+        a = arr[j];
+        if(a > b){
+            b = a;
+        }
     }
+    return b;
 }
-console.log("largest element = " + greatele);
+console.log("largest element = " + great(arr));
